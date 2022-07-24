a7c2305b79150d550dbada329b49879c35346d89
princigupta037 <princygupta037@gmail.com>
2022-07-24 21:35:21 +0530

    event Emitter to create dir and add date,uuid

---
+++ src/Backend/eventLog.js
@@ -0,0 +1,15 @@
+
+
+const logEvents = require('./eventEmitter');
+
+const EventEmitter = require('events');
+
+class MyEmitter extends EventEmitter { };
+
+const myEmitter = new MyEmitter();
+
+myEmitter.on('log', (msg) => logEvents(msg));
+
+setTimeout(()=>{
+    myEmitter.emit('log', 'Log Event Emitted!')
+},2000);
